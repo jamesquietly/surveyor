ff6ed220faca88b794b2c49e6ad76973ce1ade27
jamesquietly <jamesquietly@gmail.com>
2018-03-02 14:13:22 -0800

    finished auth on header

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,11 +10,17 @@ module.exports = app => {
     );
 
     //user comes back with a code after permission
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get(
+        '/auth/google/callback', 
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys');
+        }
+    );
 
     app.get('/api/logout', (req, res) => {
         req.logout();
-        res.send(req.user);
+        res.redirect('/');
     });
 
     app.get('/api/current_user', (req, res) => {
--- services/passport.js
+++ services/passport.js
@@ -25,19 +25,16 @@ passport.use(
         callbackURL: '/auth/google/callback',
         proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-        //async query with .then promise
-        User.findOne({ googleId: profile.id }).then(existingUser => {
-            //there is existing user with this id in mongoDB
-            if(existingUser) {
-                done(null, existingUser);
-            }
-            //no existing user
-            else {
-                new User({ googleId: profile.id })
-                    .save()
-                    .then(user => done(null, user));
-            }
-        });
+    async (accessToken, refreshToken, profile, done) => {
+        //async query that returns a promise
+        const existingUser = await User.findOne({ googleId: profile.id })
+        //there is existing user with this id in mongoDB
+        if(existingUser) {
+            done(null, existingUser);
+        }
+        else {
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);
+        }
     })
 );
